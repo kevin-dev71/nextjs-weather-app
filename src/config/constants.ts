5201770ff92ffd070c615251bdbd1b355db7f3c5
Kevin Dev <kevin.developer71@gmail.com>
2022-09-14 19:11:47 -0600

    types: config constants and types declaration

---
+++ src/config/constants.ts
@@ -0,0 +1,6 @@
+export const OPENWEATHER_API_URL =
+  process.env.OPENWEATHER_API_BASE_URL || "http://api.openweathermap.org";
+
+export const OPENWEATHER_API_KEY = process.env.OPENWEATHER_API_KEY || "";
+
+export const DEFAULT_CITY = process.env.DEFAULT_CITY || "santiago de chile";
